29da8f812d2b9bf7fff8a73d6657136ae6c58e52
Alson33 <garbuja12alson@gmial.com>
2021-10-09 20:53:31 +0545

    add book model schema

---
+++ backend/models/book.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [ true, 'Book name is required' ],
+    },
+    author: {
+        type: String,
+        required: [ true, 'Author name is required' ]
+    },
+    availableNumber: {
+        type: Number,
+        required: [ true, 'Available number is required' ],
+        default: 0,
+    },
+    genres: {
+        type: Array,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('books', bookSchema)
