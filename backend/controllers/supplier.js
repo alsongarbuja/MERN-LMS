19054ec780a5b134e83ab1ba2a929cb52416f499
Alson33 <garbuja12alson@gmial.com>
2021-10-10 15:02:23 +0545

    add supplier controller functions

---
+++ backend/controllers/supplier.js
@@ -0,0 +1,41 @@
+
+const Supplier = require('../models/supplier')
+const { StatusCodes } = require('http-status-codes')
+
+const getAllSuppliers = async (req, res) => {
+    const suppliers = await Supplier.find({}).sort('-createdAt')
+    res.status(StatusCodes.OK).json({ suppliers, suppliersCount: suppliers.length })
+}
+
+const getSupplier = async (req, res) => {
+    const supplier = await Supplier.findById({ _id: req.params.id })
+    res.status(StatusCodes.OK).json({ supplier })
+}
+
+const addSupplier = async (req, res) => {
+    await Supplier.create(req.body)
+    res.Status(StatusCodes.CREATED).json('Supplier Added Successfully')
+}
+
+const updateSupplier = async (req, res) => {
+    await Supplier.findByIdAndUpdate({
+        _id: req.params.id
+    }, req.body, {
+        new: true,
+        runValidators: true
+    })
+    res.status(StatusCodes.OK).json('Supplier Updated Successfully')
+}
+
+const deleteSupplier = async (req, res) => {
+    await Supplier.findByIdAndRemove({ _id: req.params.id })
+    res.status(StatusCodes.OK).json('Supplier Deleted Successfully')
+}
+
+module.exports = {
+    getAllSuppliers,
+    getSupplier,
+    addSupplier,
+    updateSupplier,
+    deleteSupplier
+}
