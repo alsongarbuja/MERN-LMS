35af87e73fe12f83075e102e8696fd8d72e008f1
Alson33 <garbuja12alson@gmial.com>
2021-10-10 15:02:09 +0545

    add supplier model schema

---
+++ backend/models/supplier.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose')
+
+const supplierSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [ true, 'Supplier name is required' ]
+    },
+    contact : {
+        type: Number,
+        required: [ true, 'Contact number is required' ],
+        maxlength: 10,
+        minlength: 10,
+    },
+    address: {
+        type: String,
+        required: [ true, 'Address is required' ]
+    },
+}, {
+    timestamps: true,
+})
+
+module.exports = mongoose.model('suppliers', supplierSchema)
