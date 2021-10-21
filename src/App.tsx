66f2a21787f57a4248df156e05002c1e4459ae59
Alson33 <garbuja12alson@gmial.com>
2021-10-21 17:42:56 +0545

    add route for home, register and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import styles from './App.module.css'
 
@@ -8,27 +8,43 @@ import Sidebar from './components/sidebar/sidebar';
 import Borrow from './components/borrows/borrow';
 import BookIndex from './components/books';
 import SupplierIndex from './components/suppliers';
+import Home from './components/pages/home/Home';
+import Register from './components/pages/register/Register';
+import Login from './components/pages/login/Login';
 
 function App() {
   return (
     <Router>
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <div className={styles.main}>
-          <Navbar />
-          <Route path="/borrows">
-              <Borrow />
-          </Route>
-          <Route path="/fines">
-            {/* <Fines /> */}
-          </Route>
-          <BookIndex />
-          <Route path="/users">
-            {/* <User /> */}
-          </Route>
-          <SupplierIndex />
-        </div>
-      </div>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <div className={styles.wrapper}>
+            <Sidebar />
+            <div className={styles.main}>
+              <Navbar />
+              <Route path="/borrows">
+                  <Borrow />
+              </Route>
+              <Route path="/fines">
+                {/* <Fines /> */}
+              </Route>
+              <BookIndex />
+              <Route path="/users">
+                {/* <User /> */}
+              </Route>
+              <SupplierIndex />
+            </div>
+          </div>
+        </Route>
+      </Switch>
     </Router>
   );
 }
